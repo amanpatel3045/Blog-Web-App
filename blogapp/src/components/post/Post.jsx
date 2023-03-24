8b22be44699a88afa280de95a0863890387f9b67
amanpatel3045 <amanpatel3045@gmail.com>
2023-03-24 23:29:38 +0530

    aligned posts in structured manner

--- blogapp/src/components/post/Post.jsx
+++ blogapp/src/components/post/Post.jsx
@@ -3,7 +3,11 @@ import "./post.css";
 export default function post() {
   return (
     <div className="post">
-      <img className="postImg" src="https://render.fineartamerica.com/images/rendered/search/print/8/8/break/images/artworkimages/medium/1/watching-the-sun-christian-marcel.jpg" alt="" />
+      <img
+        className="postImg"
+        src="https://render.fineartamerica.com/images/rendered/search/print/8/8/break/images/artworkimages/medium/1/watching-the-sun-christian-marcel.jpg"
+        alt=""
+      />
       <div className="postInfo">
         <div className="postCats">
           <span className="postCat">Music</span>
@@ -13,6 +17,20 @@ export default function post() {
         <hr />
         <span className="postDate">1 hour ago</span>
       </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nam
+        natus laborum veritatis architecto, animi aperiam non dolorum! Dolorum
+        nisi ratione magni veniam. Excepturi officiis repellendus, tempore eius
+        voluptatem recusandae.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nam
+        natus laborum veritatis architecto, animi aperiam non dolorum! Dolorum
+        nisi ratione magni veniam. Excepturi officiis repellendus, tempore eius
+        voluptatem recusandae.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nam
+        natus laborum veritatis architecto, animi aperiam non dolorum! Dolorum
+        nisi ratione magni veniam. Excepturi officiis repellendus, tempore eius
+        voluptatem recusandae.
+      </p>
     </div>
   );
 }
