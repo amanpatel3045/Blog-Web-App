4338db746481d1ade3b03a7510ffb03acdfbe3f6
amanpatel3045 <amanpatel3045@gmail.com>
2023-03-26 00:35:29 +0530

    using Route for home page

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -1,20 +1,19 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      {/* <Home/> */}
-      {/* <Single/> */}
-      {/* <Write/> */}
-      {/* <Settings /> */}
-   <Login/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
