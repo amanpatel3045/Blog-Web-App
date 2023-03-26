dfcdc9529faa6e6e946ecfa8e91a5556c59555ec
amanpatel3045 <amanpatel3045@gmail.com>
2023-03-26 23:39:46 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.listen("5000",()=>{
-    console.log("backend is running");
-})
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen("5000", () => {
+  console.log("backend is running");
+});
