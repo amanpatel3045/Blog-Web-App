f4b44fe8a2a0f13ee165390aecb4286a2169f434
amanpatel3045 <amanpatel3045@gmail.com>
2023-03-29 23:31:48 +0530

    create, update done of post

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 app.use(express.json());
@@ -20,7 +20,9 @@ mongoose
   });
 
 app.use("/api/auth", authRoute);
-app.use("/api/users",userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts",postRoute);
+
 app.listen("5000", () => {
   console.log("backend is running");
 });
